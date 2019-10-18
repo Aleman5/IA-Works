6921b9b11d64b4a16b2c05912db32601516a1709
Alejandro-san <ale.bros@hotmail.com>
2019-10-18 20:45:46 -0300

    Added Flocking Project

---
+++ Flocking/Assets/Scripts/Boid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public float speed;
+    public float sightLenght;
+    public float rotSpeed;
+
+    void Update()
+    {
+        //Vector3 objDir = FlockingManager.Instance.CalculateDirectionObjective(this);
+        //transform.forward = Vector3.Slerp(transform.forward, objDir, rotSpeed * Time.deltaTime);
+    }
+}
---
+++ Flocking/Assets/Scripts/FlockingManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockingManager : MonoBehaviour
+{
+    // Hacerlo Singleton
+
+    public Vector3 CalculateDirectionObjective(Boid thisBoid)
+    {
+        Vector3 dir = new Vector3();
+
+        List<Transform> adyBoids = FlockingLogic.GetBoidsInRange(thisBoid.transform.position, thisBoid.sightLenght);
+
+        dir = FlockingLogic.GetDirectionObjective(thisBoid.transform, adyBoids);
+
+        return dir;
+    }
+}
